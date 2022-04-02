0915e4a47467d284332cb5f5dd616e2db4fa791b
Stuart Blavatnik <stuartb@magenic.com>
2022-04-02 15:30:57 -0400

    Initial commit

---
+++ src/main/java/com/twoforboth/Rectangle.java
@@ -0,0 +1,37 @@
+package com.twoforboth;
+
+public final class Rectangle {
+
+    private double top;
+    private double left;
+    private double height;
+    private double width;
+
+    public Rectangle(double top, double left, double height, double width) {
+        this.top = top;
+        this.left = left;
+        this.height = height;
+        this.width = width;
+    }
+
+    public static boolean intersects(Rectangle otherRectangle) {
+
+        boolean intersects = false;
+
+        return intersects;
+    }
+
+    public static boolean contains(Rectangle otherRectangle) {
+
+        boolean contains = false;
+
+        return contains;
+    }
+
+    public static boolean isAdjacent(Rectangle otherRectangle) {
+
+        boolean isAdjacent = false;
+
+        return isAdjacent;
+    }
+}
---
+++ src/test/java/com/twoforboth/AppTest.java
@@ -0,0 +1,20 @@
+package com.twoforboth;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
